20f0a81e50adc91af99b99966519c093f4f3d2ac
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-17 19:21:53 -0500

    made server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const db = require('./models');
+const mongoose = require('mongoose');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(require("./controllers/api.js"));
+app.use(require("./controllers/main-routes.js"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }
+)
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
+
+module.exports = db;
