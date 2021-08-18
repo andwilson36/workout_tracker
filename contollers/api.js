ec83bd8ec28eaa346dd1f0afd9a9a501d7128ec8
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-17 19:16:24 -0500

    added routes

---
+++ contollers/api.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const path = require('path');
+const Workout = require("../models/Workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration",
+            },
+        },
+    },])
+        .then((workoutdb) => {
+            res.json(workoutdb);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+        .then((workoutdb) => {
+            res.json(workoutdb);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+
+    router.put("/api/workouts/:id", ({ params, body }, res) => {
+
+        Workout.findOneAndUpdate({ _id: params.id }, { $push: { exercises: body } }, { new: true })
+            .then((workoutdb) => {
+                res.json(workoutdb);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
+
+    router.get("/api/workouts/range", (req, res) => {
+        Workout.aggregate([{
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration",
+                },
+            },
+        },])
+            .sort({ _id: -1 })
+            .limit(7)
+            .then((workoutdb) => {
+                console.log(workoutdb);
+                res.json(workoutdb);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
+
+});
+
+module.exports = router;
